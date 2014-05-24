029c5eaeb8e02ae8747a950e56fd67e1426e8f48
Keegan <kbailey.v@gmail.com>
2014-05-23 18:34:41 -0700

    db config file

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Keegan
+ * Date: 23/05/14
+ * Time: 6:20 PM
+ *  PDO
+ */
+
+$constring = 'mysql:host=dota.keeganbailey.com;dbname=dotakeeg_admin';
+$user = "dotakeeg_admin";
+$password = "dota10";
