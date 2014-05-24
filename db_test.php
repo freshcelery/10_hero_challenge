0a8109536be24aa28e4ba87e4d2c7302ca6966fd
Keegan <kbailey.v@gmail.com>
2014-05-23 19:03:20 -0700

    having DB object is useless in PDO interactions, test file works 100%

--- db_test.php
+++ db_test.php
@@ -6,14 +6,21 @@
  * Time: 6:35 PM
  */
 
-require_once 'obj/db.php';
 
-$db = new db();
+$db = new PDO('mysql:host=localhost;dbname=dotakeeg_admin;charset=utf8', 'dotakeeg_admin', 'dota10');
 
 try {
     $stmnt = "select * from ladder";
-    $x = $db->getData($stmnt);
-    print_r($stmnt);
+    $x = getData($stmnt, $db);
+    foreach($x as $i){
+        echo "{$i['steam_id']} = {$i['points']} <br>";
+    }
 } catch(PDOException $ex) {
     echo $ex->getMessage();
+}
+
+
+function getData($_in, $db) {
+    $stmt = $db->query($_in);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
