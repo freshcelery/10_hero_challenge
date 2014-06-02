23c7bd780200194cbef35bc5b4320c4a2a120d8f
Cory Purnell <cpurnell91@gmail.com>
2014-06-02 01:36:52 -0700

    Took out php functions and put them elsewhere, added jquery ajax calls for reroll functionality

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ session_start();
 
 include 'obj/openid.php';
 include 'apikey.php';
+include 'index_functions.php';
 include_once 'obj/user.php';
 include_once 'obj/hero.php';
 
@@ -61,110 +62,9 @@ if (isset($_SESSION['SteamID64'])) {
 
 }
 
-//function that checks DB for user for the first time. if no there, creates.
-function check_db_for_first_login($_user, mysqli $mysqli){
-    try{
-        //save username and id to vars
-        $id = substr($_user['response']['players'][0]['steamid'], 3) - 61197960265728;
-        $points = 0;
-        $name = $_user['response']['players'][0]['personaname'];
-
-        $result = $mysqli->query("SELECT * FROM ladder WHERE steam_id = $id");
-
-        if (!$result) {
-            die($mysqli->error);
-        }
-
-        if (! $result->num_rows > 0) {
-            $result = $mysqli->query("INSERT IGNORE INTO ladder (steam_id,points,name) VALUES ('$id', '$points', '$name')");
-        }
-
-    } catch(mysqli_sql_exception $e) {
-        echo $e->getMessage();
-    }
-}
-
-function generate_history_table(mysqli $mysqli){
-    $id = substr($_SESSION['SteamID64'], 3) - 61197960265728;
-
-    //get needed things from DB
-    $result = $mysqli->query("SELECT hero_id_string, seq_id, is_done FROM hero WHERE steam_id = $id ORDER BY seq_id DESC");
-
-    if (!$result) {
-        die($mysqli->error);
-    }
-
-    //for each row returned in query pull outi nfo needed, then create hero objects for every ID. make the table data and print it till out of rows.
-    while ($row = mysqli_fetch_assoc($result)) {
-        $seq_id = $row['seq_id'];
-        $hero_id_string = $row['hero_id_string'];
-        $is_done = $row['is_done'];
-
-        $hero_id_array = explode(",", $hero_id_string);
-
-        $image_text = '';
-        foreach($hero_id_array as $hero_id){
-            if(!empty($hero_id)){
-                $hero = new hero($hero_id);
-                $image_text .= '<div class="span1"><img src="'. $hero->get_image() .'" class="img-polaroid"></div>';
-            }
-        }
-
-        //This can be changed if we add a completed timestamp in, then I can have
-        //time completed and in progress
-        if($is_done){
-            $is_done = "Completed";
-        } else {
-            $is_done = "In Progress";
-        }
-        echo "<tr>
-                 <td>{$seq_id}</td>
-                 <td>{$image_text}</td>
-                 <td>{$is_done}</td>
-              </tr>";
-    }
-}
-
-function generate_current_hero_table(){
-    if(($_SESSION['SteamID64'] > '2147483647')){
-        $SteamID_ = $_SESSION['SteamID64'];
-    
-
-        $current_user = new user($SteamID_);
-
-        $current_heroes = $current_user->get_hero_list();
-        if(isset($current_heroes)){
-            foreach($current_heroes as $hero_id => $completed){
-                if($hero_id > 0){
-                    $hero_obj = new hero($hero_id);
-                    if($completed == 0){
-                        echo"<div class='span2'><img src='".$hero_obj->get_image()."' class='img-polaroid'></div>";
-                    }
-                    else{
-                        echo"<div class='span2'><img src='".$hero_obj->get_image()."' class='img-polaroid completed'></div>";
-                    }
-                }
-            }
-        }
-    }
-}
-
-function make_reroll_button(){
-    if(($_SESSION['SteamID64'] > '2147483647')){
-        $SteamID_ = $_SESSION['SteamID64'];
-    
-
-        $current_user = new user($SteamID_);
-
-        $reroll = $current_user->get_reroll_available();
-        if($reroll == 1){
-            echo "<input type='button' class='reroll' value='Reroll Heroes' />";
-        }
-    }
-}
-
-
 ?>
+
+
 <head>
     <!-- styles -->
     <link href="css/bootstrap.css" rel="stylesheet">
@@ -179,12 +79,28 @@ function make_reroll_button(){
             $('.loading').fadeOut('slow');
             $('.jumbotron').fadeIn('slow');
         })// end load
-        /*
+        
         $(function(){
             $('.reroll').click(function(){
-                    //Do AJAX here
+                $('.loading').html("<h1> Rerolling your incomplete heroes! </h1>");
+
+                //Hide content and show load screen
+                $('.jumbotron').fadeOut('slow');
+                $('.loading').fadeIn('slow');
+
+                //Get steam id stored in hidden variable
+                var steamID_64 = $('.steam_id').val(); 
+
+                // Call index_function with ajax and pass params to call reroll heroes
+                $.get('index_functions.php', {action: 'reroll', steam_id: steamID_64}, update_heroes);
+                return false;
             });//end click
-        }); // end document ready */
+
+            function update_heroes(data){
+                //Refresh the page
+                location.reload(true);
+            }
+        }); // end document ready 
     </script>
 </head>
 <body data-spy="scroll" data-target=".bs-docs-sidebar" style="padding:40px;">
@@ -236,11 +152,11 @@ function make_reroll_button(){
                 echo "<div class=\"span1 offset10\"><h4>Points: {$ladder_results['points']}</h4></div>";
                 echo "<div class=\"span5\"><img src=\"{$user['response']['players'][0]['avatarfull']}\" class=\"img-polaroid\"></div>";
                 echo '<div class="span12"><hr></div>';
-                echo '<div class="span12"><h5>Your 10 heroes</h5>';
-                generate_current_hero_table();
+                echo '<div class="span12 heroes_div"><h5>Your 10 heroes</h5>';
+                generate_current_hero_table($_SESSION['SteamID64']);
                 echo'</div>';
                 echo '<div class="span12">';
-                make_reroll_button();
+                make_reroll_button($_SESSION['SteamID64']);
                 echo '</div>';
 
                 echo "<div class=\"span12\"><h5>History</h5></div>
@@ -251,7 +167,7 @@ function make_reroll_button(){
                             <th>Heroes</th>
                             <th>Completed On</th>
                         </tr>";
-                generate_history_table($mysqli);
+                generate_history_table($mysqli, $_SESSION['SteamID64']);
                 echo "</table>";
                 echo "</div></div>";
 
